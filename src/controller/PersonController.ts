376efd4d66a585f014df0d15a3ddfd67c6b23c12
maria <cristinabelen97@gmail.com>
2023-02-03 12:04:07 -0500

    listar, crear, actualizar y eliminar de la tabla person

--- src/controller/PersonController.ts
+++ src/controller/PersonController.ts
@@ -1,31 +1,52 @@
 import { Request, Response } from "express";
-import { BAD_REQUEST, SUCCESS_STATUS } from "../core/constant";
-import { GetPerson , CreatePerson, DeletePerson,UpdatePerson, } from "../repository/PersonRepository";
-import prisma from "../connection/prisma";
+import { SUCCESS_STATUS, BAD_REQUEST } from "../core/constant";
+import {
+  GetPerson,
+  CreatePerson,
+  DeletePerson,
+  UpdatePerson
+} from "../repository/PersonRepository";
 
 class PersonHandler {
   public async GetPerson(_req: Request, res: Response): Promise<Response> {
     const data = await GetPerson();
-    return res.status(SUCCESS_STATUS).send({ data });
+    if (data) {
+      return res.status(SUCCESS_STATUS).send({ data });
+    } else {
+      return res.status(BAD_REQUEST);
+    }
   }
 
   public async CreatePerson(req: Request, res: Response): Promise<Response> {
-      const data = req.body
-      data.date_born_at = new Date(data.date_born_at);
-      const new_person = await CreatePerson(data);
-      return res.status(SUCCESS_STATUS).send({ new_person });    
-  }
-  public async DeletePerson(req: Request, res: Response): Promise<Response> {
-    const id = Number(req.params.id);
-    const delete_person = await DeletePerson(id);
-    return res.status(SUCCESS_STATUS).send(delete_person);
+    const data = req.body;
+    data.date_born_at = new Date(data.date_born_at);
+    const new_person = await CreatePerson(data);
+    if (new_person) {
+      return res.status(SUCCESS_STATUS).send({ data });
+    } else {
+      return res.status(BAD_REQUEST);
+    }
   }
 
   public async UpdatePerson(req: Request, res: Response): Promise<Response> {
     const id = Number(req.params.id);
-    const data = req.body
-    const update_person = await UpdatePerson(id,data);
-    return res.status(SUCCESS_STATUS).send(update_person);
+    const data = req.body;
+    const update_person = await UpdatePerson(id, data);
+    if (update_person) {
+      return res.status(SUCCESS_STATUS).send({ update_person });
+    } else {
+      return res.status(BAD_REQUEST);
+    }
+  }
+
+  public async DeletePerson(req: Request, res: Response): Promise<Response> {
+    const id = Number(req.params.id);
+    const delete_person = await DeletePerson(id);
+    if (delete_person) {
+      return res.status(SUCCESS_STATUS).send({ delete_person });
+    } else {
+      return res.status(BAD_REQUEST);
+    }
   }
 }
 
--- src/repository/PersonRepository.ts
+++ src/repository/PersonRepository.ts
@@ -1,24 +1,25 @@
 import prisma from "../connection/prisma";
+import { Person } from "../entity/Person";
 
 export async function GetPerson(): Promise<any> {
   return await prisma.instance.person.findMany();
 }
 
-export async function CreatePerson(data:any): Promise<any> {
-  const new_person = await prisma.instance.person.create({data})
-  return new_person
+export async function CreatePerson(data: Person): Promise<any> {
+  const new_person = await prisma.instance.person.create({ data });
+  return new_person;
 }
 
-export async function UpdatePerson(id_update:number,data:any): Promise<any> {
+export async function UpdatePerson(id_update: number, data: Person): Promise<any> {
   const update_person = await prisma.instance.person.update({
-    where: { id:id_update },
-    data 
-    })
-  return update_person
+    where: { id: id_update },
+    data
+  });
+  return update_person;
 }
-export async function DeletePerson(id:number): Promise<any> {
+export async function DeletePerson(id: number): Promise<any> {
   const delete_person = await prisma.instance.person.delete({
     where: { id }
-    })
-  return delete_person
+  });
+  return delete_person;
 }
