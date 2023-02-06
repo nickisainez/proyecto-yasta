76b9b4dd88955528a4d852c3eefd7525c05cbee8
emilyalvae <emilisa23@gmail.com>
2023-02-06 00:11:29 -0500

    Implementacion de logs utilizando supabase

--- src/midlewares/logger.ts
+++ src/midlewares/logger.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from "express";
+import { logMiddleware } from "../repository/LogRepository";
 
 export default (req: Request, res: Response, next: NextFunction): void => {
   const dt = new Date().toLocaleString();
   const { method, path, body, statusCode } = req;
   const log = `[${dt}] ${method}:${path} (${statusCode}) ${JSON.stringify(body)}`;
-  console.info(log);
+  logMiddleware(log);
   next();
 };
